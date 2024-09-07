9ed40c6595aac7a46bdc021439c6771f8da7003f
ImNotMenduina <mynameis_menduina@outlook.com>
2024-09-06 22:28:04 -0300

    add: animal_log dict to map objects and keep it simple

---
+++ src/AnimalLog.js
@@ -0,0 +1,8 @@
+const animal_log = {
+  LEAO: { size: 3, carnivore: true, bioma: ["SAVANA"] },
+  LEOPARDO: { size: 2, carnivore: true, bioma: ["SAVANA"] },
+  CROCODILO: { size: 3, carnivore: true, bioma: ["RIO"] },
+  MACACO: { size: 1, carnivore: false, bioma: ["SAVANA", "FLORESTA"] },
+  GAZELA: { size: 2, carnivore: false, bioma: ["SAVANA"] },
+  HIPOPOTAMO: { size: 4, carnivore: false, bioma: ["SAVANA", "RIO"] },
+};
