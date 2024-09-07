bb58f4a61132105424af93c4e1aca1b99a9b0aab
ImNotMenduina <mynameis_menduina@outlook.com>
2024-09-06 23:07:27 -0300

    add: push_to_array to create details about each biome and available_biomes to check if there is at least one biome

--- src/AnimalLog.js
+++ src/AnimalLog.js
@@ -1,8 +1,22 @@
 const animal_log = {
-  LEAO: { size: 3, carnivore: true, bioma: ["SAVANA"] },
-  LEOPARDO: { size: 2, carnivore: true, bioma: ["SAVANA"] },
-  CROCODILO: { size: 3, carnivore: true, bioma: ["RIO"] },
-  MACACO: { size: 1, carnivore: false, bioma: ["SAVANA", "FLORESTA"] },
-  GAZELA: { size: 2, carnivore: false, bioma: ["SAVANA"] },
-  HIPOPOTAMO: { size: 4, carnivore: false, bioma: ["SAVANA", "RIO"] },
+  LEAO: { size: 3, carnivore: true, biome: ["SAVANA"] },
+  LEOPARDO: { size: 2, carnivore: true, biome: ["SAVANA"] },
+  CROCODILO: { size: 3, carnivore: true, biome: ["RIO"] },
+  MACACO: { size: 1, carnivore: false, biome: ["SAVANA", "FLORESTA"] },
+  GAZELA: { size: 2, carnivore: false, biome: ["SAVANA"] },
+  HIPOPOTAMO: { size: 4, carnivore: false, biome: ["SAVANA", "RIO"] },
 };
+// retorna o detalhes de cada bioma caso a espécie seja inserida
+export function push_to_array(possibilities) {
+  let recintos = [];
+  possibilities.forEach((bioma) => {
+    recintos.push(bioma.biome_details());
+  });
+  return recintos;
+}
+// retorna true caso hajam biomas disponíveis para a espécie
+export function available_biomes(recintosViaveis) {
+  return recintosViaveis.length > 0;
+}
+
+export default animal_log;
